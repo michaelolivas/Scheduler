6901d7c60fd1939ac140b46dc62ab852d3acf837
Leo Peralta <leoperalta@Leos-MacBook-Pro.local>
2019-11-09 15:19:52 -0800

    solid main

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import json
 import sys, getopt
 import numpy as np
 from Parser import Parser
@@ -7,28 +5,34 @@ from Parser import Parser
 
 def Tasks(inputFile):
     tasks = [line.strip('\r\n').split(' ') for line in open(inputFile)]
-    print(tasks[1])
-    return tasks
+    work = [Parser(tasks[x]) for x in range(len(tasks))]
+    return tasks, work
         
 
 def main(argv):
     inputFile = ''
-    
+    sched = ''
+    EE = False 
+    task = []
+    work = []
+
     try:
-        opts, arg = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, arg = getopt.getopt(argv,"hi:s:e", ["help","inFile=", "schedule=", "energy"])
     except getopt.GetoptError:
-        print ('test.py -i <inputfile> -o <outputfile>')
+        print ('test.py -i <inputfile> -s <edf/rm> -e')
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
+        if opt in ("-h", "--help"):
+            print ('test.py -i <inputfile> -s <edf/rm> -e')
             sys.exit()
-        elif opt in ("-i", "--ifile"):
+        if opt in ("-i", "--ifile"):
             inputFile = arg
+        if opt in ("-s", "--schedule"):
+            sched = arg
+        if opt in ("-e", "--energy"):
+            EE = True
 
-    tasks = Tasks(inputFile)
-    W1 = Parser(tasks[1])
-    print(W1)
+    tasks , work = Tasks(inputFile)
 
 if __name__== "__main__":
   main(sys.argv[1:])

5a1eeb52ff10e31d6a73ee23207dedd9b6ff8ca4
LRPeralta <leo.raul.peralta@gmail.com>
2019-11-08 19:09:10 -0800

    started input recongition

--- Parser.py
+++ Parser.py
@@ -1,8 +1,36 @@
 import pandas as pd
+import json
+import sys, getopt
 
-def main():
-  print("Hello World!")
-  txt = pd.read_csv('input1.txt')
-  print(txt)  
+
+class Parser:
+    def __init__(self, input_txt):
+        self.input_txt = input_txt
+        txt = [line.rstrip('\n') for line in open(input_txt)]
+        W1 = txt[1]
+        W1 =W1.split("w1")[-1]
+        W1 = W1.split()
+        print(W1)  
+
+def main(argv):
+    inputFile = ''
+    
+    try:
+        opts, arg = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputFile = arg
+
+    print(inputFile)
+    test = Parser(inputFile)
+
+
 if __name__== "__main__":
-  main()
+  main(sys.argv[1:])
+

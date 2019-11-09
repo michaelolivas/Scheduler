35177e1c4683bcdc20b52292c84448c7a67204a3
Leo Peralta <leoperalta@Leos-MacBook-Pro.local>
2019-11-09 12:13:11 -0800

    Task Parser Check

--- Parser.py
+++ Parser.py
@@ -4,33 +4,15 @@ import sys, getopt
 
 
 class Parser:
-    def __init__(self, input_txt):
-        self.input_txt = input_txt
-        txt = [line.rstrip('\n') for line in open(input_txt)]
-        W1 = txt[1]
-        W1 =W1.split("w1")[-1]
-        W1 = W1.split()
-        print(W1)  
+    def __init__(self, task):
+        self.task = str(task[0])
+        self.deadline = int(task[1])
+        self.wcet1188 = int(task[2])
+        self.wcet918 = int(task[3])
+        self.wcet648 = int(task[4])
+        self.wcet384 = int(task[5]) 
 
-def main(argv):
-    inputFile = ''
-    
-    try:
-        opts, arg = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print ('test.py -i <inputfile> -o <outputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print ('test.py -i <inputfile> -o <outputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputFile = arg
-
-    print(inputFile)
-    test = Parser(inputFile)
-
-
-if __name__== "__main__":
-  main(sys.argv[1:])
+    def __str__(self):
+        return ('Task: {0}  Deadline: {1}  WCET1188: {2}  WCET918: {3}  WCET648: {4}  WCET384: {4}'
+                .format(self.task, self.deadline, self.wcet1188, self.wcet918, self.wcet648, self.wcet384))
 

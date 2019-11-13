c9a3f935991b596c5c8bc31c72bedb5b350d6899
LRPeralta <leo.raul.peralta@gmail.com>
2019-11-12 18:36:04 -0800

    switching users

--- main.py
+++ main.py
@@ -20,34 +20,45 @@ def sort_edf(q):
 def edf(tasks, header):
     q = queue(tasks)
     num = len(q)
-    power = header.power1188
-    print(power)
-    for sec in range(header.Exetime): 
-        idle = False       
+    power = header.power1188 * 0.001
+    exe_time = 0
+    for sec in range(header.Exetime):
+        idle = False 
+        new_process = False      
         q = sort_edf(q)
         pull = []
         for i in range(num):
             process = q.pop()
+            if(sec == 0 ): previous_process = process
+            if(previous_process == process):
+                break
+            if (previous_process != process):
+                new_process = True
+                previous_process = process
             if(process.entry > sec):
                 pull.append(process)
                 if(i == num-1):
                     idle = True
                     break
-                
             if(process.entry <= sec):
                 pull.append(process)
                 break
+        if(previous_process == process and not new_process):
+            exe_time = exe_time + 1
+        if(new_process):
+            previous_process.runTime = previous_process.runTime + 1
+            print(sec, previous_process.task, exe_time)
+            exe_time = 0
         if(idle):
             q = pull + q 
             print("{0} IDLE {1}".format(sec, sec+1))
             continue
-        if(process.entry <= sec):
-            process.runTime = process.runTime + 1
-            print(sec, process.task, process.runTime+sec)
+
         if (process.runTime == process.wcet1188):
             process.runTime = 0
             process.entry = process.deadline
             process.deadline = process.deadline + process.period
+
         q = pull + q
 
 

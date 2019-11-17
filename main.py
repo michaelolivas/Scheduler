8c72f6acdcf9ec5c56bda4fe8c45f1a19c36a2da
Leo Peralta <leo.raul.peralta@gmail.com>
2019-11-17 12:39:31 -0800

    EDF Release

--- main.py
+++ main.py
@@ -14,59 +14,76 @@ def queue(tasks):
     return q
 
 def sort_edf(q):
-    q.sort(key=operator.attrgetter("deadline", "wcet1188","entry"), reverse=True)
+    q.sort(key=operator.attrgetter("deadline","entry"), reverse=True)
     return q
 
 def edf(tasks, header):
     q = queue(tasks)
+    non_ready_task = []
     num = len(q)
-    power = header.power1188 * 0.001
-    exe_time = 0
-    start_time = 0
-    for sec in range(header.Exetime):
-        idle = False 
-        new_process = False      
-        q = sort_edf(q)
-        pull = []
-        for i in range(num):
-            process = q.pop()
-            if(sec == 0): 
-                start_time = sec
-                previous_process = process
-            if(process.entry <= sec):
-                pull.append(process)
-                if (previous_process != process):
-                    new_process = True
-                break
-            if(process.entry > sec):
-                pull.append(process)
-                if(i == num-1):
-                    idle = True
-                    if(previous_process.entry<sec): new_process = True
-                    break
-        if(previous_process == process and not new_process):
-            previous_process.runTime = previous_process.runTime + 1
-            exe_time = exe_time + 1
-        if(new_process):
-            previous_process.runTime = previous_process.runTime + 1
-            exe_time = exe_time + 1
-            exe_power = power * exe_time
-            print(start_time+1, previous_process.task, exe_time, exe_power)
-            start_time = sec
-            exe_time = 0
-            previous_process = process
+    entry = 0
+    next_entry = 0
+    idle = False
+    exec_time = 0
+    seconds = 0
+    #Runs for the entire execution time desired
+    while seconds < header.Exetime:
+        #If it's Idle, stay Idle until next available entry
         if(idle):
-            q = pull + q 
-            print("{0} IDLE {1}".format(sec, sec+1))
-            continue
+            if(seconds < next_entry): 
+                seconds += 1
+                continue 
+        #Reset for checking purposes     
+        idle = False
+        q = sort_edf(q)
+        non_ready_task = []
+        #Sorts through the queue in order to see which task to run next
+        for x in range(num):
+            process = q.pop()
+            if(seconds == 0): next_process = process
+            if(x == 0): next_entry = process.entry
+            #Checks if it's time for the process to be executed
+            if(process.entry <= seconds):
+                process.runTime = process.runTime + 1
+                exec_time +=1  
+                #Checks if the process has reached it's execution time
+                if(next_process.runTime == next_process.wcet1188):
+                    print("{0} {1} 1188 {2} {3}".format(entry+1, process.task, exec_time, header.power1188*exec_time))
+                    entry = entry+exec_time
+                    seconds = entry
+                    exec_time = 0
+                    process.runTime = 0
+                    process.entry += process.period
+                    process.deadline = process.deadline + process.period
+                    non_ready_task.append(process)
+                    break
+                #Checks if a new process is being executed
+                elif(next_process != process): 
+                    if(exec_time != 1):
+                        exec_time -=1
+                        print("{0} {1} 1188 {2} {3}".format(entry+1, next_process.task, exec_time, header.power1188*exec_time))
+                        entry = entry+exec_time
+                        seconds = entry
+                        exec_time = 1
+                    next_process = process
+                    non_ready_task.append(process)
+                    break
+                non_ready_task.append(process)
+                break
+            #If the process next in the que is not ready
+            if(process.entry > seconds):
+                #Adds to the not ready list, and on  the top of the loop pops the next process 
+                non_ready_task.append(process)
+                #If it has checked the whole queue then, all asks are not ready. Therefore, Idle.
+                if(x == num -1): 
+                    print ("{0} IDLE IDLE {1} {2}".format(seconds, next_entry - seconds, header.idle*(next_entry - seconds)))
+                    entry = next_entry - 1
+                    idle = True
+        seconds += 1
+        #Returns the tasks that were popped to the not ready list back to the queue 
+        q = non_ready_task + q
+        
 
-        if (process.runTime == process.wcet1188):
-            process.runTime = 0
-            process.entry = process.deadline
-            process.deadline = process.deadline + process.period
-            
-
-        q = pull + q
 
 
 def main(argv):
@@ -74,6 +91,7 @@ def main(argv):
     sched = ''
     EE = False 
 
+    #Gathers all argument the user desires to use
     try:
         opts, arg = getopt.getopt(argv,"hi:s:e", ["help","inFile=", "schedule=", "energy"])
     except getopt.GetoptError:
@@ -90,8 +108,10 @@ def main(argv):
         if opt in ("-e", "--energy"):
             EE = True
     
+    #Parses the input file of listed tasks 
     tasks, header = parse_tasks(inputFile)
     
+
     if(sched == 'edf'):
         edf(tasks, header)
     
